6c9411eaab597bbd01fe8ef957b3f357c477093f
david <ostermann.david@gmail.com>
2018-02-02 17:52:03 +0100

    rename roletype to role

---
+++ app/test/pwd.test.js
@@ -0,0 +1,34 @@
+const { encode, compare } = require("../auth/pwd");
+
+test('PWD encode', () => {
+  return encode('coucou')
+  .then( hash => expect(hash).toBeDefined() )
+})
+
+test("PWD encode empty password generate error", () => {
+  return encode()
+  .then( t => console.log('t : ', t))
+  .catch(err => { 
+    return expect(err).toBeDefined()
+  });
+});
+
+// promise style
+test("PWD compare true", () => {
+  return encode("coucou")
+    .then(hash => compare("coucou", hash))
+    .then(isMatch => expect(isMatch).toBe(true));
+})
+
+// async await style
+// test('PWD compare true', async () => {
+//   const hash = await encode('coucou')
+//   const isMatch = await compare('coucou', hash)
+//   expect(isMatch).toBe(true)
+// })
+
+test("PWD compare false", () => {
+  return encode("coucou")
+    .then(hash => compare("kiki", hash))
+    .then(isMatch => expect(isMatch).toBe(false))
+});
---
+++ app/test/user.test.js
@@ -0,0 +1,47 @@
+const user = require('../models/user') 
+
+test("getUsers results is defined", () => {
+  return user.getUsers().then(results => expect(results).toBeDefined());
+});
+
+test("getUsers has at least one item", () => {
+  return user
+    .getUsers()
+    .then(results => expect(results.length).toBeGreaterThan(0));
+});
+
+test("getUsers return an array", () => {
+  return user
+    .getUsers()
+    .then(results => expect(Array.isArray(results)).toBe(true));
+});
+
+test('noExists return true for coucou@coucou.fr', () => {
+  return user.noExists('coucou@coucou.fr').then(
+    bool => expect(bool).toBe(true)
+  );
+})
+
+test('getUserByEmail return a user object', () => {
+  return user.getUserByEmail('do@do.do')
+  .then( data => expect(data.firstname).toBeDefined())
+});
+
+test("getUserByEmail return an error for fake email", () => {
+  return user.getUserByEmail("fake@do.do").then( data => expect(data).toBe(false))
+});
+
+test("getUserById return a user object", () => {
+  return user.getUserById(1).then(data => {
+    return expect(data.firstname).toBeDefined();
+  });
+});
+
+test("getUserById return an error for fake ID", () => {
+  return user.getUserById(999).then( data => expect(data).toBe(false))
+});
+
+test('createUser insert un user', () => {
+  return user.createUser({firstname: 'Alexandra', lastname: 'Leveille', email: 'al@do.do', password: 'coucou'})
+  .then( data => expect(data.rowCount).toBe(1))
+})
